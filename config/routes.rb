d798bad228f5d1958680b0bf0e6428e88e7aa4f5
Rayan84 <rayan_84@live.com>
2022-03-02 11:56:53 +0100

    Add name attribute to user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  #   get 'users/sign_out' => '/'
+  #   #delete '/users/sign_out' => 'destroy_user_session_path'
+  # end
 
+  # devise_for :users do
+  #   delete '/users/sign_out' => 'devise/sessions#destroy'
+  # end
   # Defines the root path route ("/")
   # root "articles#index"
 end
